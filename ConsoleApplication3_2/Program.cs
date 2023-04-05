7de0524edb39b3d372a57b8c7d52b6e1fc7cc845
MRX <kurganovss666@gmail.com>
2023-04-05 21:46:46 +0300

    Test \n and \t

--- ConsoleApplication3_2/Program.cs
+++ ConsoleApplication3_2/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApplication3_2
         {
             const string MyName = "Alexandr";
             Console.WriteLine(MyName);
+            Console.WriteLine("I am \n 23 years old");
+            Console.WriteLine("\t I live in St. Petersburg");
             Console.ReadKey();
         }
     }
