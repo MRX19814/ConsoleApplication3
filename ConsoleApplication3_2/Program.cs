6d99bf9c34101c7655e63fa0500ce96f7ba3cb86
MRX <kurganovss666@gmail.com>
2023-04-05 21:34:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Alexandr";
+            Console.WriteLine(MyName);
+        }
+    }
+}
