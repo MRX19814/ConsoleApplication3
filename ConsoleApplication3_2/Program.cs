f31c6ae96357e10bc00e7918dbc53fad677a4b87
MRX <kurganovss666@gmail.com>
2023-04-08 17:04:58 +0300

    The answer to a task where the user could enter his name himself

--- ConsoleApplication3_2/Program.cs
+++ ConsoleApplication3_2/Program.cs
@@ -23,15 +23,19 @@ namespace ConsoleApplication3_2
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
+
+            Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            byte age = checked ((byte) int.Parse(Console.ReadLine()));
+            
+
+            var age = int.Parse(Console.ReadLine());
             Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-            Console.WriteLine("What is your favorite day of week? ");
-            DayOfWeek day = (DayOfWeek) int.Parse((Console.ReadLine()));
+
+            Console.Write("What is your favorite day of week? ");
+
+            var day = (DayOfWeek)int.Parse(Console.ReadLine());
             Console.WriteLine("Your favorite day is {0}", day);
-            Console.ReadKey();
+
         }
     }
 }
