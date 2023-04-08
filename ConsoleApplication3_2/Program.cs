e49cc8b47ee68b60edf5bd1ccb0266c918eb17b6
MRX <kurganovss666@gmail.com>
2023-04-08 16:48:57 +0300

    Added response to 3.7.4

--- ConsoleApplication3_2/Program.cs
+++ ConsoleApplication3_2/Program.cs
@@ -23,8 +23,14 @@ namespace ConsoleApplication3_2
     {
         public static void Main(string[] args)
         {
-            double result = 10 % 3;
-            Console.WriteLine(result);
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked ((byte) int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.WriteLine("What is your favorite day of week? ");
+            DayOfWeek day = (DayOfWeek) int.Parse((Console.ReadLine()));
+            Console.WriteLine("Your favorite day is {0}", day);
             Console.ReadKey();
         }
     }
